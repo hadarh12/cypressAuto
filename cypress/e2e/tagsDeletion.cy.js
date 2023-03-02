0c1f8698b308c740a0181c5f7c6eeb7de24a4d1f
hadar hadad <hadar49681@gmail.com>
2023-03-02 11:21:45 +0200

    make delete tag code prettier

--- cypress/e2e/tagsDeletion.cy.js
+++ cypress/e2e/tagsDeletion.cy.js
@@ -1,23 +1,26 @@
 import defaultValue from "../fixtures/default.json";
 
 describe("delete tag", () => {
-  it("Add 2 tags and delete 1 - check that the counter increased by 1", function () {
+  it("Add 2 tags and delete 1 - check that the counter increased by 1", () => {
     cy.addTag("ironSource,game{enter}");
     cy.deleteTag("ironSource");
+
     cy.counter().should(
       "have.text",
       defaultValue.maxTags - defaultValue.tagsAmount - 1
     );
   });
 
-  it("Add tag and delete it - the tag is not part of the tags list", function () {
+  it("Add tag and delete it - the tag is not part of the tags list", () => {
     cy.addTag("ironSource{enter}");
     cy.deleteTag("ironSource");
+
     cy.tagsList().contains("ironSource").should("not.exist");
   });
 
-  it("remove all", function () {
+  it("remove all", () => {
     cy.deleteAll();
+    
     cy.tagsList().should("have.length", null);
   });
 });
