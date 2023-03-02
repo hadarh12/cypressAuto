088fcfc633b406dea798845d6058205dc8aa66da
hadar hadad <hadar49681@gmail.com>
2023-03-02 11:21:00 +0200

    Add tags Insertion test and make the code prettier

--- cypress/e2e/tagsDefaultState.cy.js
+++ cypress/e2e/tagsDefaultState.cy.js
@@ -2,13 +2,14 @@ import elements from "../fixtures/elements.json";
 import defaultValue from "../fixtures/default.json";
 
 describe("defultState", () => {
-  it("Check that the counter is set to (max tags - default tags)", function () {
+  it("Check that the counter is set to (max tags - default tags)", () => {
     cy.counter().should("have.text", defaultValue.maxTags - defaultValue.tagsAmount);
   });
 
-  it("add tags -  reload page - the default tags show.", function () {
+  it("add tags -  reload page - the default tags show.", () => {
     cy.addTag("ironSource,game{enter}");
     cy.reload();
+
     cy.tagsList()
       .invoke("text")
       .then((text) => {
@@ -17,19 +18,21 @@ describe("defultState", () => {
     cy.tagsList().should("have.length", defaultValue.tagsAmount);
   });
 
-  it("add tags - reload page - update counter to default tags amount", function () {
+  it("add tags - reload page - update counter to default tags amount", () => {
     cy.addTag("ironSource,game{enter}");
     cy.reload();
+
     cy.counter().should("have.text", defaultValue.maxTags - defaultValue.tagsAmount);
   });
 
-  it("delete one of the default tags, reload page - check that there are only 2 tags.", function () {
+  it("delete one of the default tags, reload page - check that there are only 2 tags.", () => {
     cy.deleteTag("javascript");
     cy.reload();
+    
     cy.tagsList().should("have.length", defaultValue.tagsAmount);
   });
 
-  it("instruction text ", function () {
+  it("instruction text ", () => {
     cy.get(elements.instructionText).should(
       "have.text",
       defaultValue.instructionTextExpect
--- cypress/e2e/tagsInsertion.cy.js
+++ cypress/e2e/tagsInsertion.cy.js
@@ -1,49 +1,15 @@
 import elements from "../fixtures/elements.json";
 import defaultValue from "../fixtures/default.json";
 
-describe("add tag", () => {
+describe("Tags insertion ", () => {
 
-  describe("long tag", () => {
-    beforeEach(() => {
-      cy.addLongTag(100);
-    });
-
-    it("Tags longer than the component framework's width should not be allowed", function () {
-      cy.tagsContainer.then((frameworkElement) => {
-        cy.tagsList().then((tagsListElement) => {
-          const frameworkwidth = frameworkElement[0].getBoundingClientRect();
-          const tagListwidth =
-            tagsListElement[defaultValue.tagsAmount].getBoundingClientRect();
-
-          expect(tagListwidth.width).to.be.lessThan(frameworkwidth.width);
-        });
-      });
-    });
-
-    it("Insert tag longer than the component framework – The tag splits into several lines suitable for framework", function () {
-      cy.get("ul").then((frameworkElement) => {
-        cy.tagsList().then((tagsListElement) => {
-          const frameworkRect = frameworkElement[0].getBoundingClientRect();
-          const tagElement = tagsListElement.eq(defaultValue.tagsAmount);
-          const tagRect = tagElement[0].getBoundingClientRect();
-
-          expect(tagRect.top).to.be.greaterThan(frameworkRect.top);
-          expect(tagElement[0].clientWidth).to.be.lessThan(
-            frameworkElement[0].clientWidth
-          );
-          expect(tagElement.css("white-space")).to.equal("normal");
-        });
-      });
-    });
-  });
-
-  it("Add tag - check that the tag was added", function () {
+  it("Add tag - check that the tag was added.", () => {
     cy.addTag("ironSource{enter}");
 
     cy.tagsList().should("include.text", "ironSource");
   });
 
-  it("Add several tags together - check that they were added", function () {
+  it("Add several tags together - check that they were added.", () => {
     cy.addTag("ironSource,game,play{enter}");
 
     cy.tagsList()
@@ -52,104 +18,139 @@ describe("add tag", () => {
       .should("contain.text", "play");
   });
 
-  it("add single tag - check if the counter decreased", function () {
+  it("Add single tag - check if the counter decreased.", () => {
     cy.addTag("game{enter}");
+
     cy.counter().should(
       "have.text",
       defaultValue.maxTags - defaultValue.tagsAmount - 1
     );
   });
 
-  it("add several tags - check if the counter decreased", function () {
+  it("Add several tags - check if the counter decreased.", () => {
     cy.addTag("game,ironSource,play{enter}");
+
     cy.counter().should(
       "have.text",
       defaultValue.maxTags - defaultValue.tagsAmount - 3
     );
   });
-  //think how to make that func in commands
-  it("add 11 tags one by one - not allowed", function () {
+
+  it("Add 11 tags one by one - not allowed.", () => {
     for (let i = 0; i <= defaultValue.maxTags - defaultValue.tagsAmount; i++) {
       cy.addTag(`${i}${i}{enter}`);
     }
+
     cy.tagsList().should("have.length", defaultValue.maxTags);
   });
 
-  it("add 10 tags one by one - allowed", function () {
+  it("Add 10 tags one by one - allowed.", () => {
     for (let i = 0; i < defaultValue.maxTags - defaultValue.tagsAmount; i++) {
       cy.addTag(`${i}${i}{enter}`);
     }
+
     cy.tagsList().should("have.length", defaultValue.maxTags);
   });
 
-  it("add 11 tags together - not allowed", function () {
-    let tagListToCheck = [];
-    for (let i = 0; i < defaultValue.maxTags - defaultValue.tagsAmount + 1; i++) {
-      tagListToCheck.push(`${i}${i}`);
-    }
-    const tagsString = tagListToCheck.join(",");
-    cy.addTag(tagsString + "{enter}");
-    cy.tagsList().should("have.length", defaultValue.maxTags);
-  });
+  describe("Add 11 tags together", () => {
+    beforeEach(() => {
+        let tagListToCheck = [];
+        for (let i = 0; i < defaultValue.maxTags - defaultValue.tagsAmount + 1; i++) {
+        tagListToCheck.push(`${i}${i}`);
+        }
+        const tagsString = tagListToCheck.join(",");
+        cy.addTag(tagsString + "{enter}");
+        });
 
-  it("add 11 tags together - the first tag won’t be added", function () {
-    let tagListToCheck = [];
-    for (let i = 0; i < defaultValue.maxTags - defaultValue.tagsAmount + 1; i++) {
-      tagListToCheck.push(`${i}${i}`);
-    }
-    const tagsString = tagListToCheck.join(",");
-    cy.addTag(tagsString + "{enter}");
-    cy.tagsList().should("not.include.text", tagListToCheck[0]);
-  });
-  //split?
-  it("add one char tag - the tag won’t be counted and added", function () {
+        it("Add 11 tags together - not allowed.", () => {
+            cy.tagsList().should("have.length", defaultValue.maxTags);
+        });
+
+        it("Add 11 tags together - the first tag won’t be added.", () => {
+            cy.tagsList().should("not.include.text", tagListToCheck[0]);
+        });
+    });  
+
+  it("Add one char tag - the tag won’t be added.", () => {
     cy.addTag("£{enter}");
+
     cy.tagsList().should("not.include.text", "£");
     cy.counter().should("have.text", defaultValue.maxTags - defaultValue.tagsAmount);
   });
-  //split?
-  it("add a one char tag as a part of several tags - the tag with on char wont count and wont added", function () {
+
+  it("Add one char tag - the tag won’t be counted.", () => {
+    cy.addTag("£{enter}");
+
+    cy.counter().should("have.text", defaultValue.maxTags - defaultValue.tagsAmount);
+  });
+
+  it("Add a one char tag as a part of several tags - the tag with one char wont added.", () => {
     cy.addTag("£,game{enter}");
-    cy.counter().should(
-      "have.text",
-      defaultValue.maxTags - defaultValue.tagsAmount - 1
-    );
+
     cy.tagsList().should("not.include.text", "£");
   });
-  it("add tag that contains one char and a space - the tag will not be saved", function () {
-    cy.addTag(" £ {enter}");
-    cy.tagsList().should("not.include.text", "£ ");
-  });
-  it("add a one char tag as a part of several tags that contains one char and a space - the tag will not be saved", function () {
-    cy.addTag(" £ ,game{enter}");
-    cy.tagsList().should("not.include.text", "£ ");
-  });
 
-  it("add a tag and check if the input field empty", function () {
-    cy.addTag("ironSource{enter}");
-    cy.get(elements.input).should("be.empty");
-  });
+  it("Add a one char tag as a part of several tags - the tag with one char wont count.", () => {
+    cy.addTag("£,game{enter}");
 
-  it("add a several tags together and check if the input field empty", function () {
-    cy.addTag("ironSource,game,play{enter}");
-    cy.get(elements.input).should("be.empty");
-  });
-
-  it("add the same tag, one by one - check that only one added and counted", function () {
-    cy.addTag("ironSource{enter}");
-    cy.addTag("ironSource{enter}");
-    cy.tagsList().should("have.length", +defaultValue.tagsAmount + 1);
-    cy.tagsList().contains("ironSource").should("have.length", 1);
     cy.counter().should(
       "have.text",
       defaultValue.maxTags - defaultValue.tagsAmount - 1
     );
   });
-  //split?
-  it("add the same tag name as a string split by comma - check that only one of the duplicitous tag added and counted", function () {
+
+  it("Add tag that contains one char and a space - the tag will not be saved.", () => {
+    cy.addTag(" £ {enter}");
+
+    cy.tagsList().should("not.include.text", "£ ");
+  });
+
+  it("Add a one char tag as a part of several tags that contains one char and a space - the tag will not be saved.", () => {
+    cy.addTag(" £ ,game{enter}");
+
+    cy.tagsList().should("not.include.text", "£ ");
+  });
+
+  it("Add a tag and check if the input field empty.", () => {
+    cy.addTag("ironSource{enter}");
+
+    cy.get(elements.input).should("be.empty");
+  });
+
+  it("Add a several tags together and check if the input field empty.", () => {
+    cy.addTag("ironSource,game,play{enter}");
+
+    cy.get(elements.input).should("be.empty");
+  });
+
+  it("Add the same tag, one by one - check that only one added.", () => {
+    cy.addTag("ironSource{enter}");
+    cy.addTag("ironSource{enter}");
+
+    cy.tagsList().should("have.length", +defaultValue.tagsAmount + 1);
+    cy.tagsList().contains("ironSource").should("have.length", 1);
+  });
+
+  it("Add the same tag, one by one - check that only one counted.", () => {
+    cy.addTag("ironSource{enter}");
+    cy.addTag("ironSource{enter}");
+
+    cy.counter().should(
+      "have.text",
+      defaultValue.maxTags - defaultValue.tagsAmount - 1
+    );
+  });
+  
+  it("Add the same tag name as a string split by comma - check that only one of the duplicitous tag added.", () => {
     cy.addTag("game,game,ironSource{enter}");
+
     cy.tagsList().should("have.length", +defaultValue.tagsAmount + 2);
     cy.tagsList().contains("game").should("have.length", 1);
+  });
+
+  it("Add the same tag name as a string split by comma - check that only one of the duplicitous tag counted.", () => {
+    cy.addTag("game,game,ironSource{enter}");
+
     cy.counter().should(
       "have.text",
       defaultValue.maxTags - defaultValue.tagsAmount - 2
